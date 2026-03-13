fe1eb66c5574b440bf47999dd59e062f8921bdc1
Matthijs Berends <m.s.berends@umcg.nl>
2026-03-13 10:08:59 +0100

    toevoegen geo-data

---
+++ R/inlezen_geodata.R
@@ -0,0 +1,35 @@
+
+library(ggplot2)
+library(sf)
+
+geo_nuts3 <- readRDS("data/geo_nuts3.rds")
+
+geo_nuts3 |>
+  ggplot() +
+  geom_sf()
+
+geo_nuts3 |>
+  filter(nuts3 %in% c("Noord-Drenthe",
+                      "Noord-Friesland",
+                      "Oost-Groningen",
+                      "Overig Groningen",
+                      "Zuidoost-Drenthe",
+                      "Zuidoost-Friesland",
+                      "Zuidwest-Drenthe",
+                      "Zuidwest-Friesland")) |>
+  ggplot() +
+  geom_sf()
+
+geo_nuts3 |>
+  filter(nuts3 %in% c("Noord-Drenthe",
+                      "Noord-Friesland",
+                      "Oost-Groningen",
+                      "Overig Groningen",
+                      "Zuidoost-Drenthe",
+                      "Zuidoost-Friesland",
+                      "Zuidwest-Drenthe",
+                      "Zuidwest-Friesland")) |>
+  ggplot() +
+  geom_sf(aes(fill = oppervlakte_km2)) +
+  geom_sf_text(aes(label = nuts3), colour = "white", size = 3) +
+  theme_void()
